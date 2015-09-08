ba8950995887c42a4206384255b9b244ecfc8315
stefan <stefanhitskov@gmail.com>
2015-09-08 15:45:15 +0300

    Added toolbar

--- app/src/main/java/net/hitskov/testproject/ActivityMain.java
+++ app/src/main/java/net/hitskov/testproject/ActivityMain.java
@@ -3,16 +3,33 @@ package net.hitskov.testproject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by stefan on 08.09.15.
  */
 public class ActivityMain extends Activity{
 
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_main);
+    private Toolbar toolbar;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initTollBar();
+    }
+
+    private void initTollBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
